0a37f2cd993bc330c7030eff40ca2f9374636ad8
Christopher McArthur <prince.chrismc@gmail.com>
2017-11-01 14:23:10 -0400

    further cleaned and compressed ObjectLoader

--- NeverGreenGrove/NeverGreenGrove/src/ObjLoader.cpp
+++ NeverGreenGrove/NeverGreenGrove/src/ObjLoader.cpp
@@ -34,22 +34,21 @@ bool LoadObjFile(const char* path, std::vector<glm::vec3>* out_vertices, std::ve
 
       if (strcmp(lineHeader, "v") == 0)
       {
-         glm::vec3 vertex;
-         fscanf_s(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
-         temp_vertices.push_back(vertex);
+         float x, y, z;
+         fscanf_s(file, "%f %f %f\n", &x, &y, &z);
+         temp_vertices.emplace_back(x, y, z);
       }
       else if (strcmp(lineHeader, "vt") == 0)
       {
-         glm::vec2 uv;
-         fscanf_s(file, "%f %f\n", &uv.x, &uv.y);
-         uv.y = -uv.y; // Invert V coordinate since we will only use DDS texture, which are inverted. Remove if you want to use TGA or BMP loaders.
-         temp_uvs.push_back(uv);
+         float x, y;
+         fscanf_s(file, "%f %f\n", &x, &y);
+         temp_uvs.emplace_back(x, -y); // Invert V coordinate since we will only use DDS texture, which are inverted. Remove if you want to use TGA or BMP loaders.
       }
       else if (strcmp(lineHeader, "vn") == 0)
       {
-         glm::vec3 normal;
-         fscanf_s(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
-         temp_normals.push_back(normal);
+         float x, y, z;
+         fscanf_s(file, "%f %f %f\n", &x, &y, &z);
+         temp_normals.emplace_back(x, y, z);
       }
       else if (strcmp(lineHeader, "f") == 0)
       {
@@ -76,20 +75,10 @@ bool LoadObjFile(const char* path, std::vector<glm::vec3>* out_vertices, std::ve
    // For each vertex of each triangle
    for (unsigned int i = 0; i < vertexIndices.size(); i++)
    {
-      // Get the indices of its attributes
-      unsigned int vertexIndex = vertexIndices.at(i);
-      unsigned int uvIndex = uvIndices.at(i);
-      unsigned int normalIndex = normalIndices.at(i);
-
-      // Get the attributes thanks to the index
-      glm::vec3 vertex = temp_vertices[vertexIndex - 1];
-      glm::vec2 uv = temp_uvs[uvIndex - 1];
-      glm::vec3 normal = temp_normals[normalIndex - 1];
-
-      // Put the attributes in buffers
-      out_vertices->push_back(vertex);
-      out_uvs->push_back(uv);
-      out_normals->push_back(normal);
+      // Get the attributes in draw order thanks to the index
+      out_vertices->push_back(temp_vertices.at(vertexIndices.at(i) - 1));
+      out_uvs->push_back(temp_uvs.at(uvIndices.at(i) - 1));
+      out_normals->push_back(temp_normals.at(normalIndices.at(i) - 1));
    }
 
    return true;
