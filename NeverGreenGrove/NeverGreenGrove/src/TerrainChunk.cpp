f6f560550514cf78206a72045240200efc2bcc84
Chris McArthur <prince.chrismc@gmail.com>
2017-11-19 11:38:07 -0500

    more formatting

--- NeverGreenGrove/NeverGreenGrove/src/TerrainChunk.cpp
+++ NeverGreenGrove/NeverGreenGrove/src/TerrainChunk.cpp
@@ -64,23 +64,21 @@ void TerrainChunk::generateVertices() {
    std::random_device rd;
    std::mt19937 gen(rd());
 
-
    // max hills
    const int hill_qty = 3 + gen() % 7;
-   std::vector<float> hill_heights;
 
    // creates indicies for hill peaks
-   std::vector<int> horizontalPeaks;
-   std::vector<int> depthPeaks;
+   std::vector<int> horizontal_peaks;
+   std::vector<int> depth_peaks;
    for (int i = 0; i < hill_qty; i++)
    {
-      horizontalPeaks.emplace_back(INNER_LENGTH / hill_qty + (INNER_LENGTH / hill_qty)*i);
-      depthPeaks.emplace_back(INNER_LENGTH / hill_qty + (INNER_LENGTH / hill_qty)*i);
+      horizontal_peaks.emplace_back(INNER_LENGTH / hill_qty + (INNER_LENGTH / hill_qty)*i);
+      depth_peaks.emplace_back(INNER_LENGTH / hill_qty + (INNER_LENGTH / hill_qty)*i);
    }
 
    // shuffle the indicies
-   std::shuffle(horizontalPeaks.begin(), horizontalPeaks.end(), gen);
-   std::shuffle(depthPeaks.begin(), depthPeaks.end(), gen);
+   std::shuffle(horizontal_peaks.begin(), horizontal_peaks.end(), gen);
+   std::shuffle(depth_peaks.begin(), depth_peaks.end(), gen);
 
    // create all the hills --------------------------------------------------------------------------------------------------------------------------
    std::vector<Hill> hills;
@@ -88,58 +86,65 @@ void TerrainChunk::generateVertices() {
    // Saturate their info, create peaks
    for (int i = 0; i < hill_qty; i++)
    {
-      const float tempheight = MIN_HEIGHT + gen() % (int)(MAX_HEIGHT - MIN_HEIGHT);
-      const int max_r = std::min( { (float)horizontalPeaks.at(i), (float)depthPeaks.at(i), (float)CHUNK_LENGTH - horizontalPeaks.at(i), (float)CHUNK_LENGTH - depthPeaks.at(i), (float)MAX_RADIUS });
-      const float tempradius = MIN_RADIUS + (gen() % max_r);
+      const float temp_height = MIN_HEIGHT + gen() % (int)(MAX_HEIGHT - MIN_HEIGHT);
+      const int max_r = std::min( { (float)horizontal_peaks.at(i), (float)depth_peaks.at(i), (float)CHUNK_LENGTH - horizontal_peaks.at(i), (float)CHUNK_LENGTH - depth_peaks.at(i), (float)MAX_RADIUS });
+      const float temp_radius = MIN_RADIUS + (gen() % max_r);
 
-      grid_2d.at(horizontalPeaks.at(i)).at(depthPeaks.at(i)).y = tempheight;
-      Hill newhill(tempheight, tempradius, horizontalPeaks.at(i), depthPeaks.at(i));
-      hills.emplace_back(newhill);
-      color_2d.at(newhill.x).at(newhill.z) = glm::vec3(0.4f + 0.6*(tempheight / MAX_HEIGHT), 0.2f + 0.8*(tempheight / MAX_HEIGHT), 0.04f + 0.96*(tempheight / MAX_HEIGHT));
+      grid_2d.at(horizontal_peaks.at(i)).at(depth_peaks.at(i)).y = temp_height;
+      Hill new_hill(temp_height, temp_radius, horizontal_peaks.at(i), depth_peaks.at(i));
+      hills.emplace_back(new_hill);
+      color_2d.at(new_hill.x).at(new_hill.z) = glm::vec3(0.4f + 0.6*(temp_height / MAX_HEIGHT), 0.2f + 0.8*(temp_height / MAX_HEIGHT), 0.04f + 0.96*(temp_height / MAX_HEIGHT));
    }
 
    // Change all points inside their radius
    // to find wether a point is in a circle, use https://math.stackexchange.com/questions/198764/how-to-know-if-a-point-is-inside-a-circle
-   for (int i = 0; i < CHUNK_LENGTH; i++) {
-      for (int j = 0; j < CHUNK_LENGTH; j++) {
+   for (int i = 0; i < CHUNK_LENGTH; i++)
+   {
+      for (int j = 0; j < CHUNK_LENGTH; j++)
+      {
          //check every hills
          for (auto hill : hills) {
             float distance = sqrt((i - hill.x)*(i - hill.x) + (j - hill.z)*(j - hill.z));
-            if (distance <= hill.radius) {
+            if (distance <= hill.radius)
+            {
 
                // Using Logistic (Sigmoid) function: https://en.wikipedia.org/wiki/Logistic_function
                // clamp multiplier to 1
                float multiplier = ((hill.radius - distance) / hill.radius * 3 > 1) ? 1 : (hill.radius - distance) / hill.radius * 3;
-               float newheight = (hill.height / (1 + std::exp(-0.1*(hill.radius - distance - hill.radius*0.5)))) * multiplier;
+               float new_height = (hill.height / (1 + std::exp(-0.1*(hill.radius - distance - hill.radius*0.5)))) * multiplier;
 
-               if (newheight > grid_2d.at(i).at(j).y) {
-                  grid_2d.at(i).at(j).y = newheight;
-                  color_2d.at(i).at(j) = glm::vec3(0.4f + 0.6*(newheight / MAX_HEIGHT), 0.2f + 0.8*(newheight / MAX_HEIGHT), 0.04f + 0.96*(newheight / MAX_HEIGHT));
+               if (new_height > grid_2d.at(i).at(j).y)
+               {
+                  grid_2d.at(i).at(j).y = new_height;
+                  color_2d.at(i).at(j) = glm::vec3(0.4f + 0.6*(new_height / MAX_HEIGHT), 0.2f + 0.8*(new_height / MAX_HEIGHT), 0.04f + 0.96*(new_height / MAX_HEIGHT));
                }
             }
             // prevent spikes on hills
-            if (i == hill.x && j == hill.z) {
+            if (i == hill.x && j == hill.z)
+            {
                grid_2d.at(i).at(j).y = grid_2d.at(i - 1).at(j).y;
             }
          }
       }
    }
 
-
    grid = flatten(grid_2d);
    color = flatten(color_2d);
 }
 
 //create a simple flat terrain
-void TerrainChunk::flatTerrain() {
+void TerrainChunk::flatTerrain()
+{
    GLuint counter = 0;
    std::vector<std::vector<GLuint>> indices_2d;
 
-   for (int i = 0; i < CHUNK_LENGTH; i++) {
+   for (int i = 0; i < CHUNK_LENGTH; i++)
+   {
       std::vector<glm::vec3> temp_builder;
       std::vector<glm::vec3> temp_color;
       std::vector<GLuint> temp_indices;
-      for (int j = 0; j < CHUNK_LENGTH; j++) {
+      for (int j = 0; j < CHUNK_LENGTH; j++)
+      {
          temp_builder.emplace_back(i, 0, j);
          temp_indices.emplace_back(counter++);
          //brownish color
