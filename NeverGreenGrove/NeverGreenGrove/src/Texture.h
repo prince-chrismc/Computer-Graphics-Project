79250db925fc3dee83ca2cbaa8c0dc6241ba8575
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-11-20 02:16:11 -0500

    fixed the path for stb_image

--- NeverGreenGrove/NeverGreenGrove/src/Texture.h
+++ NeverGreenGrove/NeverGreenGrove/src/Texture.h
@@ -5,7 +5,7 @@
 
 #include "gl\glew.h"
 #define STB_IMAGE_IMPLEMENTATION
-#include <\Users\Nicholas\Programming\Computer_Graphics_Project\Computer-Graphics-Project\NeverGreenGrove\NeverGreenGrove\src\stb_image.h>
+#include "stb_image.h"
 
 class Texture
 {
