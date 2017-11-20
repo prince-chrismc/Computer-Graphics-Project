e4da211b9e996ff551ac8ab74723ac6ece0623c8
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-11-20 00:38:53 -0500

    created the function to load and create a texture

--- NeverGreenGrove/NeverGreenGrove/Texture.h
+++ NeverGreenGrove/NeverGreenGrove/Texture.h
@@ -1,1 +1,21 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
 #pragma once
+
+#include "gl\glew.h"
+#include <\Users\Nicholas\Programming\Computer_Graphics_Project\Computer-Graphics-Project\NeverGreenGrove\NeverGreenGrove\src\stb_image.h>
+
+class Texture
+{
+public:
+	Texture() {}
+	void createTexture(const char* imagePath);
+
+private:
+	int width;
+	int height;
+	int channel;
+};
+
+#endif
