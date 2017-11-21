d78dd1326c5cc49891561a1ca5b70a1f899aada3
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-11-21 01:25:26 -0500

    getting some textures on the trees. Switched the uvs and sending them as texture coordinates. Texture partially there

---
+++ NeverGreenGrove/NeverGreenGrove/src/DrawableTree.cpp
@@ -0,0 +1,209 @@
+/*
+MIT License
+
+Copyright (c) 2017   Chris McArthur, prince.chrismc(at)gmail(dot)com
+                     Daniel P, privorotskyd(at)gmail(dot)com
+                     Nicholas G, dj_nick_gattuso(at)hotmail(dot)com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "DrawableTree.h"
+#include "ObjLoader.h"
+#include "Shader.h"
+#include <random>                               //std::mt19937
+
+std::once_flag TreeA::TreeObj::s_Flag;
+std::shared_ptr<TreeA::TreeObj> TreeA::TreeObj::s_Instance;
+std::once_flag TreeB::TreeObj::s_Flag;
+std::shared_ptr<TreeB::TreeObj> TreeB::TreeObj::s_Instance;
+
+TreeA::TreeObj::TreeObj()
+{
+   auto shaderProgram = ShaderLinker::GetInstance();
+   GLuint PositonIndex = shaderProgram->GetAttributeLocation("position");
+   GLuint ColorIndex = shaderProgram->GetAttributeLocation("color");
+   //GLuint NormalIndex = shaderProgram->GetAttributeLocation("normal");
+   //GLuint UvIndex = shaderProgram->GetAttributeLocation("uv");
+   GLuint textureIndex = shaderProgram->GetAttributeLocation("textureCoordinate");
+
+   std::vector<glm::vec3> vertices;
+   std::vector<glm::vec3> colors;
+   std::vector<glm::vec3> normals;
+   std::vector<glm::vec2> uvs;
+   LoadObjFile("assets/tree_xs.obj", &vertices, &normals, &uvs); //read the vertices from the cube.obj file
+
+   m_NumVertices = (GLsizei)vertices.size();
+
+   for (glm::vec3 vert : vertices)
+   {
+      (vert.y > 2.10) ? colors.emplace_back(0.075, 0.545, 0.271) : colors.emplace_back(0.545, 0.271, 0.075);
+   }
+
+   texture.createTexture("assets/wall.jpg");
+
+
+   glGenVertexArrays(1, &m_VAO);
+   glGenBuffers(1, &m_Verticies);
+   glGenBuffers(1, &m_Colors);
+   //glGenBuffers(1, &m_Normals);
+   glGenBuffers(1, &m_Uvs);
+
+   // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+   glBindVertexArray(m_VAO);
+
+   glBindBuffer(GL_ARRAY_BUFFER, m_Verticies);
+   glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices.front(), GL_STATIC_DRAW);
+   glVertexAttribPointer(PositonIndex, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+   glEnableVertexAttribArray(PositonIndex);
+
+   glBindBuffer(GL_ARRAY_BUFFER, m_Colors);
+   glBufferData(GL_ARRAY_BUFFER, colors.size() * sizeof(glm::vec3), &colors.front(), GL_STATIC_DRAW);
+   glVertexAttribPointer(ColorIndex, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+   glEnableVertexAttribArray(ColorIndex);
+
+   //glBindBuffer(GL_ARRAY_BUFFER, m_Normals);
+   //glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), &normals.front(), GL_STATIC_DRAW);
+   //glVertexAttribPointer(NormalIndex, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+   //glEnableVertexAttribArray(NormalIndex);
+
+   glBindBuffer(GL_ARRAY_BUFFER, m_Uvs);
+   glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs.front(), GL_STATIC_DRAW);
+   glVertexAttribPointer(textureIndex, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+   glEnableVertexAttribArray(textureIndex);
+
+   glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+   glBindVertexArray(0);
+}
+
+TreeA::TreeObj::~TreeObj()
+{
+   glDeleteBuffers(1, &m_Verticies);
+   glDeleteBuffers(1, &m_Colors);
+   //glDeleteBuffers(1, &m_Normals);
+   //glDeleteBuffers(1, &m_Uvs);
+   glDeleteVertexArrays(1, &m_VAO);
+}
+
+TreeB::TreeObj::TreeObj()
+{
+   auto shaderProgram = ShaderLinker::GetInstance();
+   GLuint PositonIndex = shaderProgram->GetAttributeLocation("position");
+   GLuint ColorIndex = shaderProgram->GetAttributeLocation("color");
+   //GLuint NormalIndex = shaderProgram->GetAttributeLocation("normal");
+   //GLuint UvIndex = shaderProgram->GetAttributeLocation("uv");
+
+   std::vector<glm::vec3> vertices;
+   std::vector<glm::vec3> colors;
+   std::vector<glm::vec3> normals;
+   std::vector<glm::vec2> uvs;
+   LoadObjFile("assets/tree2_xs.obj", &vertices, &normals, &uvs); //read the vertices from the cube.obj file
+
+   m_NumVertices = (GLsizei)vertices.size();
+
+   for (glm::vec3 vert : vertices)
+   {
+      (vert.y > 2.00) ? colors.emplace_back(0.075, 0.545, 0.271) : colors.emplace_back(0.545, 0.271, 0.075);
+   }
+
+   glGenVertexArrays(1, &m_VAO);
+   glGenBuffers(1, &m_Verticies);
+   glGenBuffers(1, &m_Colors);
+   //glGenBuffers(1, &m_Normals);
+   //glGenBuffers(1, &m_Uvs);
+
+   // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+   glBindVertexArray(m_VAO);
+
+   glBindBuffer(GL_ARRAY_BUFFER, m_Verticies);
+   glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices.front(), GL_STATIC_DRAW);
+   glVertexAttribPointer(PositonIndex, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+   glEnableVertexAttribArray(PositonIndex);
+
+   glBindBuffer(GL_ARRAY_BUFFER, m_Colors);
+   glBufferData(GL_ARRAY_BUFFER, colors.size() * sizeof(glm::vec3), &colors.front(), GL_STATIC_DRAW);
+   glVertexAttribPointer(ColorIndex, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+   glEnableVertexAttribArray(ColorIndex);
+
+   //glBindBuffer(GL_ARRAY_BUFFER, m_Normals);
+   //glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), &normals.front(), GL_STATIC_DRAW);
+   //glVertexAttribPointer(NormalIndex, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+   //glEnableVertexAttribArray(NormalIndex);
+
+   //glBindBuffer(GL_ARRAY_BUFFER, m_Uvs);
+   //glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs.front(), GL_STATIC_DRAW);
+   //glVertexAttribPointer(UvIndex, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+   //glEnableVertexAttribArray(UvIndex);
+
+   glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+   glBindVertexArray(0);
+}
+
+TreeB::TreeObj::~TreeObj()
+{
+   glDeleteBuffers(1, &m_Verticies);
+   glDeleteBuffers(1, &m_Colors);
+   //glDeleteBuffers(1, &m_Normals);
+   //glDeleteBuffers(1, &m_Uvs);
+   glDeleteVertexArrays(1, &m_VAO);
+}
+
+void TreeA::Draw() const
+{
+   auto shaderProgram = ShaderLinker::GetInstance();
+   shaderProgram->SetUniformMat4("model_matrix", m_ModelMatrix);
+
+   glBindVertexArray(TreeObj::GetInstance()->GetVAO());
+   glDrawArrays(GL_TRIANGLES, 0, TreeObj::GetInstance()->GetNumberOfVertices());
+   glBindVertexArray(0);
+}
+
+void TreeB::Draw() const
+{
+   auto shaderProgram = ShaderLinker::GetInstance();
+   shaderProgram->SetUniformMat4("model_matrix", m_ModelMatrix);
+
+   glBindVertexArray(TreeObj::GetInstance()->GetVAO());
+   glDrawArrays(GL_TRIANGLES, 0, TreeObj::GetInstance()->GetNumberOfVertices());
+   glBindVertexArray(0);
+}
+
+std::unique_ptr<DrawableTree> TreeFactory::GetNewTree() {
+
+   std::random_device rd;
+   std::mt19937 g(rd());
+
+   switch (g() % 5 + 1)
+   {
+   case 1:
+      return std::make_unique<TreeA>();
+   case 2:
+      return std::make_unique<TreeA2>();
+   case 3:
+      return std::make_unique<TreeA3>();
+   case 4:
+      return std::make_unique<TreeB>();
+   case 5:
+      return std::make_unique<TreeB2>();
+   default:
+      return std::make_unique<TreeA>();
+   }
+}
---
+++ NeverGreenGrove/NeverGreenGrove/src/DrawableTree.h
@@ -0,0 +1,147 @@
+/*
+MIT License
+
+Copyright (c) 2017   Chris McArthur, prince.chrismc(at)gmail(dot)com
+                     Daniel P, privorotskyd(at)gmail(dot)com
+                     Nicholas G, dj_nick_gattuso(at)hotmail(dot)com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#pragma once
+
+#include "glm\matrix.hpp"
+#include "glm/gtc/matrix_transform.hpp"         //glm::lookAt
+#include "glm\gtx\transform2.hpp"
+#include "gl\glew.h"
+#include <mutex>
+#include "Texture.h"
+
+class DrawableTree abstract
+{
+   public:
+      DrawableTree() = default;
+
+      virtual void Draw() const = 0;
+      void Translate(glm::vec3 vec) { m_ModelMatrix = glm::translate(m_ModelMatrix, vec); }
+
+   protected:
+      void Scale(glm::vec3 vec) { m_ModelMatrix = glm::scale(m_ModelMatrix, vec); }
+      void Rotate(float angle) { m_ModelMatrix = glm::rotate(m_ModelMatrix, glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f)); }
+      void Shear(float deform_x, float deform_z) { m_ModelMatrix = glm::shearY3D(m_ModelMatrix, deform_x, deform_z); }
+
+      glm::mat4 m_ModelMatrix;
+};
+
+class TreeA : public DrawableTree
+{
+   public:
+      TreeA() { Scale(glm::vec3(2.5)); }
+      void Draw() const;
+
+   private:
+      class TreeObj
+      {
+      public:
+         ~TreeObj();
+         TreeObj(const TreeObj&) = delete;
+         TreeObj& operator=(const TreeObj&) = delete;
+
+         static std::shared_ptr<TreeObj> GetInstance() { std::call_once(s_Flag, []() { s_Instance.reset(new TreeObj()); }); return s_Instance; }
+
+         const GLuint GetVAO() const { return m_VAO; }
+         const GLsizei GetNumberOfVertices() const { return m_NumVertices; }
+
+      private:
+         TreeObj();
+
+		 Texture texture;
+
+         GLuint m_VAO;
+         GLuint m_Verticies;
+         GLuint m_Colors;
+         //GLuint m_Normals;
+         GLuint m_Uvs;
+
+         GLsizei m_NumVertices;
+
+         static std::once_flag s_Flag;
+         static std::shared_ptr<TreeObj> s_Instance;
+      };
+};
+
+class TreeA2 : public TreeA
+{
+public:
+   TreeA2() { Scale(glm::vec3(1.0, 1.1, 1.1)); Rotate(90.f); }
+};
+
+class TreeA3 : public TreeA
+{
+public:
+   TreeA3() { Shear(-0.25f, 0.1f); }
+};
+
+class TreeB : public DrawableTree
+{
+   public:
+      TreeB() { Scale(glm::vec3(2.5)); }
+      void Draw() const;
+
+   private:
+      class TreeObj
+      {
+      public:
+         ~TreeObj();
+         TreeObj(const TreeObj&) = delete;
+         TreeObj& operator=(const TreeObj&) = delete;
+
+         static std::shared_ptr<TreeObj> GetInstance() { std::call_once(s_Flag, []() { s_Instance.reset(new TreeObj()); }); return s_Instance; }
+
+         const GLuint GetVAO() const { return m_VAO; }
+         const GLsizei GetNumberOfVertices() const { return m_NumVertices; }
+
+      private:
+         TreeObj();
+
+         GLuint m_VAO;
+         GLuint m_Verticies;
+         GLuint m_Colors;
+         //GLuint m_Normals;
+         //GLuint m_Uvs;
+
+         GLsizei m_NumVertices;
+
+         static std::once_flag s_Flag;
+         static std::shared_ptr<TreeObj> s_Instance;
+      };
+};
+
+class TreeB2 : public TreeB
+{
+public:
+   TreeB2() { Scale(glm::vec3(1.1, 1.25, 0.9)); Rotate(75.0f); }
+};
+
+class TreeFactory
+{
+   public:
+      TreeFactory() = delete;
+      static std::unique_ptr<DrawableTree> GetNewTree();
+};
