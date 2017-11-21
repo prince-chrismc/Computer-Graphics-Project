6bc531246b49790ff4b7c6d2b2eb3378566a9dfe
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-11-21 01:25:26 -0500

    getting some textures on the trees. Switched the uvs and sending them as texture coordinates. Texture partially there

--- NeverGreenGrove/NeverGreenGrove/src/Texture.h
+++ NeverGreenGrove/NeverGreenGrove/src/Texture.h
@@ -10,7 +10,7 @@ class Texture
 public:
 	Texture() {}
 	void createTexture(const char* imagePath);
-	float getCoordinates();
+	//float getCoordinates();
 	unsigned int getTexture();
 
 private:
@@ -20,12 +20,12 @@ private:
 	int height;
 	int channel;
 
-	float textureCoord[] = {// texture coords
+	/*float textureCoord[] = {// texture coords
 		1.0f, 1.0f,   // top right
 		1.0f, 0.0f,   // bottom right
 		0.0f, 0.0f,   // bottom left
 		0.0f, 1.0f    // top left 
-	}
+	}*/
 };
 
 #endif
