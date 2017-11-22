5e155c8fffb113943bafa53c3de4b70b1a4b1d19
DJNGattuso <dj_nick_gattuso@hotmail.com>
2017-11-22 12:34:48 -0500

    passing uvs to the drawable, using them to set the texture for the object

--- NeverGreenGrove/NeverGreenGrove/src/DrawableObject.h
+++ NeverGreenGrove/NeverGreenGrove/src/DrawableObject.h
@@ -34,7 +34,7 @@ class DrawableObject
    public:
    DrawableObject() {}
    DrawableObject(const std::vector<glm::vec3> verticies, const std::vector<glm::vec3> colors, const std::vector<GLuint> indicies);
-   DrawableObject(const std::vector<glm::vec3> verticies, const char *texturePath, const std::vector<GLuint> indicies);
+   DrawableObject(const std::vector<glm::vec3> verticies, const std::vector<glm::vec2> uvs, const char *texturePath, const std::vector<GLuint> indicies);
    virtual void Draw(const RenderMode& render_mode) const;
    void Delete();
    unsigned int getTexture();
