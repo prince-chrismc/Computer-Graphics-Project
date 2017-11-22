b5d0263473dbbad8fe8335b9486289e49cf2d2bf
Chris McArthur <prince.chrismc@gmail.com>
2017-11-21 23:59:19 -0500

    small clean up + did some document formatting

--- NeverGreenGrove/NeverGreenGrove/src/Texture.cpp
+++ NeverGreenGrove/NeverGreenGrove/src/Texture.cpp
@@ -1,34 +1,57 @@
+/*
+MIT License
+
+Copyright (c) 2017   Chris McArthur, prince.chrismc(at)gmail(dot)com
+                     Daniel P, privorotskyd(at)gmail(dot)com
+                     Nicholas G, dj_nick_gattuso(at)hotmail(dot)com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "Texture.h"
+#include "gl\glew.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #include <iostream>
 
-void Texture::createTexture(const char* imagePath)
+void Texture::createTexture(const char* image_path)
 {
-	//modified from learnopengl.com
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
+   // modified from https://learnopengl.com/#!Getting-started/Textures
+   glGenTextures(1, &texture);
+   glBindTexture(GL_TEXTURE_2D, texture);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	unsigned char *dataTemp = stbi_load(imagePath, &width, &height, &channel, 0);
-	if (!dataTemp)
-	{
-		std::cout << "Texture loading failed. Try again!" << std::endl;
-	}
-	else {
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, dataTemp);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	
-	}
+   unsigned char* texture_buffer = stbi_load(image_path, &width, &height, &channel, 0);
+   if (!texture_buffer)
+   {
+      std::cout << "Texture loading failed. Try again!" << std::endl;
+   }
+   else
+   {
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, texture_buffer);
+      glGenerateMipmap(GL_TEXTURE_2D);
+   }
 
-	stbi_image_free(dataTemp);
+   stbi_image_free(texture_buffer);
 }
-
-unsigned int Texture::getTexture() { return texture; }
-
-//float Texture::getCoordinates() { return textureCoord[1]; }
