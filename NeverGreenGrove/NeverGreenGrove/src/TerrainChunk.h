2453fcbbf8043c7eb84c5950b55d96bc9320cca2
DanielPri <privorotskyd@gmail.com>
2017-11-21 21:31:03 -0500

    Created vertical normals

--- NeverGreenGrove/NeverGreenGrove/src/TerrainChunk.h
+++ NeverGreenGrove/NeverGreenGrove/src/TerrainChunk.h
@@ -38,7 +38,9 @@ class TerrainChunk
       static constexpr GLuint CHUNK_LENGTH = 128;
       std::vector<std::vector<glm::vec3>> grid_2d;
       std::vector<std::vector<glm::vec3>> color_2d;
-	  std::vector<glm::vec2> UV;
+	  std::vector<std::vector<glm::vec3>> normals_2d;
+	  std::vector<glm::vec2> UVs;
+	  std::vector<glm::vec3> normals;
 	  std::vector<glm::vec3> grid;
       std::vector<glm::vec3> color;
       std::vector<GLuint> indices;
