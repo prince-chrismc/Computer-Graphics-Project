cd2544ce76f744e894483e6390f81e8c891238fc
DanielPri <privorotskyd@gmail.com>
2017-11-24 23:52:09 -0500

    Added TRIANGLE_STRIPS to DrawableObject

--- NeverGreenGrove/NeverGreenGrove/src/DrawableObject.cpp
+++ NeverGreenGrove/NeverGreenGrove/src/DrawableObject.cpp
@@ -92,5 +92,13 @@ void DrawableObject::Draw(const RenderMode& render_mode) const
       glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
       glBindVertexArray(0);
       break;
+
+   case RenderMode::TRIANGLE_STRIPS:
+	   glBindVertexArray(m_VAO);
+	   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_Indicies);
+	   glDrawElements(GL_TRIANGLE_STRIP, m_NumVertices, GL_UNSIGNED_INT, NULL);
+	   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	   glBindVertexArray(0);
+	   break;
    }
 }
