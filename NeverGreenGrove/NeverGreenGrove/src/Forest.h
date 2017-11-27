9760da3a43312dd5b5194fd5c529a6023674de64
Chris McArthur <prince.chrismc@gmail.com>
2017-11-26 19:15:12 -0500

    hiding the underlying implementation of a forest

--- NeverGreenGrove/NeverGreenGrove/src/Forest.h
+++ NeverGreenGrove/NeverGreenGrove/src/Forest.h
@@ -36,120 +36,32 @@ SOFTWARE.
 #include <map>
 #include <vector>
 
-class DrawableTree abstract
-{
-   public:
-      DrawableTree() : m_ModelMatrix(1.0f) {}
-
-      virtual void Draw() const = 0;
-      void Translate(glm::vec3 vec) { m_ModelMatrix = glm::translate(m_ModelMatrix, vec); }
-
-   protected:
-      void Scale(glm::vec3 vec) { m_ModelMatrix = glm::scale(m_ModelMatrix, vec); }
-      void Rotate(float angle) { m_ModelMatrix = glm::rotate(m_ModelMatrix, glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f)); }
-      void Shear(float deform_x, float deform_z) { m_ModelMatrix = glm::shearY3D(m_ModelMatrix, deform_x, deform_z); }
-
-      glm::mat4 m_ModelMatrix;
-};
-
-class TreeA : public DrawableTree
-{
-   public:
-      TreeA() { Scale(glm::vec3(2.5)); }
-      void Draw() const;
-
-   private:
-      class TreeObj
-      {
-      public:
-         ~TreeObj();
-         TreeObj(const TreeObj&) = delete;
-         TreeObj& operator=(const TreeObj&) = delete;
-
-         static std::shared_ptr<TreeObj> GetInstance() { std::call_once(s_Flag, []() { s_Instance.reset(new TreeObj()); }); return s_Instance; }
-
-         const GLuint GetVAO() const { return m_VAO; }
-         const GLsizei GetNumberOfVertices() const { return m_NumVertices; }
-
-      private:
-         TreeObj();
-
-         GLuint m_VAO;
-         GLuint m_Verticies;
-         GLuint m_Colors;
-         //GLuint m_Normals;
-         //GLuint m_Uvs;
-
-         GLsizei m_NumVertices;
-
-         static std::once_flag s_Flag;
-         static std::shared_ptr<TreeObj> s_Instance;
-      };
-};
-
-class TreeA2 : public TreeA
-{
-public:
-   TreeA2() { Scale(glm::vec3(1.0, 1.1, 1.1)); /*Rotate(90.f);*/ }
-};
-
-class TreeA3 : public TreeA
-{
-public:
-   TreeA3() { Shear(-0.25f, 0.1f); }
-};
-
-class TreeB : public DrawableTree
-{
-   public:
-      TreeB() { Scale(glm::vec3(2.5)); }
-      void Draw() const;
-
-   private:
-      class TreeObj
-      {
-      public:
-         ~TreeObj();
-         TreeObj(const TreeObj&) = delete;
-         TreeObj& operator=(const TreeObj&) = delete;
-
-         static std::shared_ptr<TreeObj> GetInstance() { std::call_once(s_Flag, []() { s_Instance.reset(new TreeObj()); }); return s_Instance; }
-
-         const GLuint GetVAO() const { return m_VAO; }
-         const GLsizei GetNumberOfVertices() const { return m_NumVertices; }
-
-      private:
-         TreeObj();
-
-         GLuint m_VAO;
-         GLuint m_Verticies;
-         GLuint m_Colors;
-         //GLuint m_Normals;
-         //GLuint m_Uvs;
-
-         GLsizei m_NumVertices;
-
-         static std::once_flag s_Flag;
-         static std::shared_ptr<TreeObj> s_Instance;
-      };
-};
-
-class TreeB2 : public TreeB
-{
-public:
-   TreeB2() { Scale(glm::vec3(1.1, 1.25, 0.9)); /*Rotate(75.0f);*/ }
-};
-
-class TreeFactory
-{
-   public:
-      TreeFactory() = delete;
-      static std::shared_ptr<DrawableTree> GetNewTree();
-};
-
 class Forest
 {
 private:
+   class DrawableTree abstract
+   {
+      public:
+         DrawableTree() : m_ModelMatrix(1.0f) {}
+
+         virtual void Draw() const = 0;
+         void Translate(glm::vec3 vec) { m_ModelMatrix = glm::translate(m_ModelMatrix, vec); }
+
+      protected:
+         void Scale(glm::vec3 vec) { m_ModelMatrix = glm::scale(m_ModelMatrix, vec); }
+         void Rotate(float angle) { m_ModelMatrix = glm::rotate(m_ModelMatrix, glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f)); }
+         void Shear(float deform_x, float deform_z) { m_ModelMatrix = glm::shearY3D(m_ModelMatrix, deform_x, deform_z); }
+
+         glm::mat4 m_ModelMatrix;
+   };
+
+   class TreeA;
+   class TreeA2;
+   class TreeA3;
+   class TreeB;
+   class TreeB2;
+   class TreeFactory;
+
    struct Point { std::size_t x, y; };
 
    //Compare the coordinates of two Points
